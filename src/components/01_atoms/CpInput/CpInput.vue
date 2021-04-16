2fd0a9e3e0ac1659f0df43eaa99aef540f8f3ad0
Richard <thibault.richard2000@gmail.com>
2021-04-16 16:48:05 +0200

    :tada: init project

---
+++ src/components/01_atoms/CpInput/CpInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <input :class="`input--${type}`" :type="`${type}`" :placeholder="`${placeholder}`">
+</template>
+
+
+<script>
+// changer le nom de la class
+export default {
+  name: 'Input',
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    placeholder: {
+      type: String,
+      default: 'Fill the blank'
+    },
+  }
+}
+</script>
+
+
+<style lang="scss">
+@import "./CpInput.scss";
+</style>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+  components: {
+  },
+  data() {
+    return {
+      text: "test"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./Home.scss"
+</style>
